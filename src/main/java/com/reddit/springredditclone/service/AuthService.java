d9bb79e201959e9df2bf19db7f6301da427a54d7
Spencer Lepine <spencerlepine26@gmail.com>
2022-10-29 15:49:19 -0700

    create uuid verification tokens with bcrypt library

--- src/main/java/com/reddit/springredditclone/service/AuthService.java
+++ src/main/java/com/reddit/springredditclone/service/AuthService.java
@@ -2,19 +2,46 @@ package com.reddit.springredditclone.service;
 
 import com.reddit.springredditclone.model.User;
 import com.reddit.springredditclone.dto.RegisterRequest;
+import com.reddit.springredditclone.model.VerificationToken;
+import com.reddit.springredditclone.repository.UserRepository;
+import com.reddit.springredditclone.repository.VerificationTokenRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.util.UUID;
 
 @Service
+@AllArgsConstructor
 public class AuthService {
 
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
+
+    @Transactional
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
         user.setUsername(registerRequest.getUsername());
         user.setEmail(registerRequest.getEmail());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
         user.setCreated(Instant.now());
         user.setEnabled(false);
+
+        userRepository.save(user);
+
+        String token = generateVerificationToken(user);
+    }
+
+    private void generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+
+        verificationTokenRepository.save(verificationToken);
+        return token;
     }
 }
