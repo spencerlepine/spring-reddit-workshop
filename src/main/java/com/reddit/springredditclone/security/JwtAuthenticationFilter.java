63d4f7d180e1ae041fb7f3affe1b6b4304751d12
Spencer Lepine <spencerlepine26@gmail.com>
2022-10-31 09:43:10 -0700

    complete JWT auth endpoints

---
+++ src/main/java/com/reddit/springredditclone/security/JwtAuthenticationFilter.java
@@ -0,0 +1,31 @@
+package com.reddit.springredditclone.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws SecurityException {
+        String jwt = getJwtFromRequest(request);
+        jwtProvider.validateToken(jwt);
+    }
+
+    private String getJwtFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer: ")) {
+            return bearerToken.substring(7);
+        }
+        return bearerToken;
+    }
+}
