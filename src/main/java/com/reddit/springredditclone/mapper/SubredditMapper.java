b842734f5e7b134a6d1b5ce2e0903595b3aa161f
Spencer Lepine <spencerlepine26@gmail.com>
2022-10-31 11:25:16 -0700

    use MapStruct library to generate methods from model to dto dynamically

---
+++ src/main/java/com/reddit/springredditclone/mapper/SubredditMapper.java
@@ -0,0 +1,23 @@
+package com.reddit.springredditclone.mapper;
+
+import com.reddit.springredditclone.dto.SubredditDto;
+import com.reddit.springredditclone.model.Post;
+import com.reddit.springredditclone.model.Subreddit;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface SubredditMapper {
+
+    @Mapping(target = "numberOfPosts", expression = "java(mapPosts(subreddit.getPosts()))")
+    SubredditDto mapSubredditToDto(Subreddit subreddit);
+
+    default Integer mapPosts(List<Post> numberOfPosts) { return numberOfPosts.size(); };
+
+    @InheritInverseConfiguration
+    @Mapping(target = "posts", ignore = true)
+    Subreddit mapDtoToSubreddit(SubredditDto subredditDto);
+}
--- src/main/java/com/reddit/springredditclone/service/SubredditService.java
+++ src/main/java/com/reddit/springredditclone/service/SubredditService.java
@@ -1,6 +1,7 @@
 package com.reddit.springredditclone.service;
 
 import com.reddit.springredditclone.dto.SubredditDto;
+import com.reddit.springredditclone.mapper.SubredditMapper;
 import com.reddit.springredditclone.model.Subreddit;
 import com.reddit.springredditclone.repository.SubredditRepository;
 import lombok.AllArgsConstructor;
@@ -17,28 +18,19 @@ import java.util.stream.Collectors;
 public class SubredditService {
 
     private final SubredditRepository subredditRepository;
+    private final SubredditMapper subredditMapper;
+
 
 
     @Transactional
     public SubredditDto save(SubredditDto subredditDto) {
-        Subreddit save = subredditRepository.save(mapSubredditDto(subredditDto));
+        Subreddit save = subredditRepository.save(subredditMapper.mapDtoToSubreddit(subredditDto));
         subredditDto.setId(save.getId());
         return subredditDto;
     }
 
     @Transactional(readOnly = true)
     public List<SubredditDto> getAll() {
-        return subredditRepository.findAll().stream().map(this::mapToDto).collect(Collectors.toList());
-    }
-
-    private SubredditDto mapToDto(Subreddit subreddit) {
-        return SubredditDto.builder().name(subreddit.getName())
-                .id(subreddit.getId())
-                .numberOfPosts(subreddit.getPosts().size())
-                .build();
-    }
-
-    private Subreddit mapSubredditDto(SubredditDto subredditDto) {
-        return Subreddit.builder().name(subredditDto.getName()).description(subredditDto.getDescription()).build();
+        return subredditRepository.findAll().stream().map(subredditMapper::mapSubredditToDto).collect(Collectors.toList());
     }
 }
