e7523f9b8338c144bb575eb45916ea34cf046469
Spencer Lepine <spencerlepine26@gmail.com>
2022-10-31 09:53:35 -0700

    inject SubredditService into SubredditController for POST request

--- src/main/java/com/reddit/springredditclone/controller/SubredditController.java
+++ src/main/java/com/reddit/springredditclone/controller/SubredditController.java
@@ -1,8 +1,12 @@
 package com.reddit.springredditclone.controller;
 
 import com.reddit.springredditclone.dto.SubredditDto;
+import com.reddit.springredditclone.model.Subreddit;
+import com.reddit.springredditclone.service.SubredditService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class SubredditController {
 
-    @PostMapping
-    public void createSubreddit(@RequestBody SubredditDto subreddit) {
+    private final SubredditService subredditService;
 
+    @PostMapping
+    public ResponseEntity<SubredditDto> createSubreddit(@RequestBody SubredditDto subredditDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(subredditService.save(subredditDto));
     }
 }
